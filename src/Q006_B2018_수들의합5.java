cc021b602caa047b9521054ff2760146bf290b7c
eunja2 <ej0514@hanmail.net>
2023-05-10 21:41:42 +0900

    006 수들의합5

---
+++ src/Q006_B2018_수들의합5.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Q006_B2018_수들의합5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int count = 1;
+        int start_index = 1;
+        int end_index = 1;
+        int sum = 1;
+
+        while(end_index != N){
+            if(sum == N) {
+                count++;
+                end_index++;
+                sum += end_index;
+            } else if(sum>N){
+                sum -= start_index;
+                start_index++;
+            } else {
+                end_index++;
+                sum += end_index;
+            }
+        }
+        System.out.println(count);
+    }
+}
