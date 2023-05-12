0818dd5d7881106dc4dba0dd4e399a5e83869edb
eunja2 <ej0514@hanmail.net>
2023-05-12 21:49:56 +0900

    015 수정렬1

---
+++ src/Q015_B2750_수정렬1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Q015_B2750_수정렬1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        for(int i=0;i<N;i++)
+            arr[i] = sc.nextInt();
+
+        for(int i=0;i<N-1;i++)
+            for(int j=0;j<N-1-i;j++)
+                if(arr[j]>arr[j+1]) {
+                    int tmp;
+                    tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+
+        for(int i=0;i<N;i++)
+            System.out.println(arr[i]);
+    }
+}
