4a4a9426280a29bc1f921f16fc8dc627124a9a06
eunja2 <ej0514@hanmail.net>
2023-05-12 14:44:36 +0900

    013 카드2

---
+++ src/Q013_B2164_카드2.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Q013_B2164_카드2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        Queue<Integer> list = new LinkedList<>();
+
+        for(int i=1;i<=N;i++)
+            list.add(i);
+
+        while(list.size()>1){
+            list.poll();
+            list.add(list.poll());
+        }
+        System.out.println(list.poll());
+    }
+}
