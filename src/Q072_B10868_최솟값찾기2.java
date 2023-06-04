518db7a78df2bdb76e5ecf9d5a195b2518bc12fd
eunja2 <ej0514@hanmail.net>
2023-06-04 20:42:11 +0900

    072 최솟값 찾기 2

---
+++ src/Q072_B10868_최솟값찾기2.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Q072_B10868_최솟값찾기2 {
+    static long[] tree;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int treeHeight = 0;
+        int length = N;
+        while(length!=0){
+            length /= 2;
+            treeHeight++;
+        }
+        int treeSize = (int)Math.pow(2, treeHeight+1);
+        int treeStart = (int)Math.pow(2, treeHeight);
+        tree = new long[treeSize+1];
+        for(int i=0;i<tree.length;i++)
+            tree[i] = Integer.MAX_VALUE;
+
+        for(int i =treeStart;i<treeStart+N;i++)
+            tree[i] = Long.parseLong(br.readLine());
+        setTree(treeSize-1);
+
+        for(int i=0;i<M;i++){
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken()) + treeStart - 1;
+            int e = Integer.parseInt(st.nextToken()) + treeStart - 1;
+            System.out.println(getMin(s,e));
+        }
+    }
+
+    private static long getMin(int s, int e){
+        long Min = Long.MAX_VALUE;
+        while(s<=e) {
+            if (s % 2 == 1) {
+                Min = Math.min(Min, tree[s]);
+                s++;
+            }
+            if (e % 2 == 0) {
+                Min = Math.min(Min, tree[e]);
+                e--;
+            }
+            s /= 2;
+            e /= 2;
+        }
+        return Min;
+    }
+    private static void setTree(int i){
+        while(i!=1){
+            if(tree[i/2]>tree[i]) tree[i/2] = tree[i];
+            i--;
+        }
+    }
+}
