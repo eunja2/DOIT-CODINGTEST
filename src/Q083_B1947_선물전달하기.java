b06823093d033ae55f5162fa13bf56bc96e6c916
eunja2 <ej0514@hanmail.net>
2023-06-07 14:34:15 +0900

    no message

--- src/Q083_B1947_선물전달하기.java
+++ src/Q083_B1947_선물전달하기.java
@@ -5,12 +5,12 @@ public class Q083_B1947_선물전달하기 {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         long MOD = 1000000000;
-        long[] D = new long[N+1];
+        long[] D = new long[1000001];
 
         D[1] = 0;
         D[2] = 1;
         for(int i=3;i<=N;i++)
-            D[i] = (i-1)*(D[i-2]+D[i-1]) % MOD;
+            D[i] = (i-1)*(D[i-1]+D[i-2]) % MOD;
         System.out.println(D[N]);
     }
 }
